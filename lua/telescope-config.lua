418c30b92c3d175fec0176843c2543a08fa11a85
Martin <martin@bech.io>
2023-01-14 15:48:26 +0100

    removed telescope hoogle

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,12 +1,8 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-telescope.load_extension'hoogle'
-
 vim.keymap.set('n', 'ff', builtin.git_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
-
-vim.keymap.set('n', 'fh', telescope.extensions.hoogle.list, {})
 
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 vim.keymap.set('n', 'gs', builtin.lsp_dynamic_workspace_symbols, {})

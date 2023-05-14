eab6bf73572d7f9252438f97efddb15bb66c3ed1
Martin <martin@bech.io>
2023-05-14 16:26:36 +0200

    telescope basic find files

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,7 +1,7 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', 'ff', builtin.git_files, {})
+vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
